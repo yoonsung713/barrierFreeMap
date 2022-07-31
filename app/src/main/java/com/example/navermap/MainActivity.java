222419760c3e4b366589eab1e70eb856720ccfae
yoonsung713 <sandyj0713@naver.com>
2022-08-01 00:13:58 +0900

    execute marker

--- app/src/main/java/com/example/navermap/MainActivity.java
+++ app/src/main/java/com/example/navermap/MainActivity.java
@@ -1,40 +1,120 @@
 package com.example.navermap;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.CameraPosition;
+import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.NaverMapOptions;
 import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.InfoWindow;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.overlay.Overlay;
 import com.naver.maps.map.util.FusedLocationSource;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Objects;
 
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
     private FusedLocationSource locationSource;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FusedLocationSource locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
+        locationSource =
+                new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
 
+        CameraPosition cameraPosition = new CameraPosition(
+                new LatLng(37.297753,126.835452), // 대상 지점
+                16, // 줌 레벨
+                20, // 기울임 각도
+                0 // 베어링 각도
+        );
 
+        NaverMapOptions options = new NaverMapOptions()
+                .camera(cameraPosition)
+                .mapType(NaverMap.MapType.Terrain)
+                .enabledLayerGroups(NaverMap.LAYER_GROUP_BUILDING)
+                .compassEnabled(true)
+                .scaleBarEnabled(true)
+                .locationButtonEnabled(true);
+
+        FragmentManager fm = getSupportFragmentManager();
+        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance(options);
+            fm.beginTransaction().add(R.id.map, mapFragment).commit();
+        }
+        mapFragment.getMapAsync(this);
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            @NonNull String[] permissions,  @NonNull int[] grantResults) {
-        FusedLocationSource locationSource;
-
+        if (locationSource.onRequestPermissionsResult(
+                requestCode, permissions, grantResults)) {
+            return;
+        }
         super.onRequestPermissionsResult(
                 requestCode, permissions, grantResults);
     }
 
-}
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        naverMap.setLocationSource(locationSource);
+        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
 
+        InfoWindow infoWindow = new InfoWindow();
+
+        Marker marker1 = new Marker();
+        Marker marker2 = new Marker();
+        Marker marker3 = new Marker();
+
+        marker1.setPosition(new LatLng(37.2984234,126.8344228));
+        marker1.setMap(naverMap);
+        marker2.setPosition(new LatLng(37.2986965,126.8372123));
+        marker2.setMap(naverMap);
+        marker3.setPosition(new LatLng(37.2968196,126.8350928));
+        marker3.setMap(naverMap);
+
+
+        marker1.setTag("학생복지관관");
+       marker1.setOnClickListener(overlay -> {
+            // 마커를 클릭할 때 정보창을 엶
+            infoWindow.open(marker1);
+            return true;
+        });
+
+        marker2.setTag("제2과학기술관\n ♿ 가능");
+        marker2.setOnClickListener(overlay -> {
+            // 마커를 클릭할 때 정보창을 엶
+            infoWindow.open(marker2);
+            return true;
+        });
+
+        marker3.setTag("학술정보관");
+        marker3.setOnClickListener(overlay -> {
+            // 마커를 클릭할 때 정보창을 엶
+            infoWindow.open(marker3);
+            return true;
+        });
+
+        infoWindow.setAdapter(new InfoWindow.DefaultTextAdapter(this) {
+            @NonNull
+            @Override
+            public CharSequence getText(@NonNull InfoWindow infoWindow) {
+                // 정보 창이 열린 마커의 tag를 텍스트로 노출하도록 반환
+                return (CharSequence) Objects.requireNonNull(infoWindow.getMarker().getTag());
+            }
+        });
+
+    }
+}
